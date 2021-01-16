b2dae0447ca3de6d399cb95220a56e757f9d3899
Tandy Nguyen <nguyetan001@gmail.com>
2021-01-16 09:19:02 -0500

    Add Redis

--- server/index.js
+++ server/index.js
@@ -1,17 +1,39 @@
-require('newrelic');
+// require('newrelic');
 require('dotenv').config();
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const fetch = require('node-fetch');
+const redis = require('redis');
 
 const app = express();
+
+const client = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PW,
+});
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/photos/:productid/:styleid', createProxyMiddleware({
+
+const getFromRedis = (req, res, next) => {
+  client.get(`${Number(req.params.productid)}-${req.params.styleid}`, (err, data) => {
+    if(err) throw err;
+    if(data !== null) {
+      client.expire(`${Number(req.params.productid)}-${req.params.styleid}`, 600);
+
+      const photos = JSON.parse(data);
+      res.send([photos.photo_urls.main_photo.regular_url, ...photos.photo_urls.other_photos])
+    } else {
+      next();
+    }
+  });
+}
+
+app.use('/photos/:productid/:styleid', getFromRedis, createProxyMiddleware({
   target: process.env.PHOTO_GALLERY_URL,
   changeOrigin: true
 }))
